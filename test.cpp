8e64a323056817ed66afea07b23044826dad9f11
Tobias.S <sieber.tobias8@gmail.com>
2022-10-11 15:59:33 +0200

    Merge Sort

--- test.cpp
+++ test.cpp
@@ -1,18 +1,109 @@
 #include <iostream>
-#include <vector>
-
-std::vector<int> manis(std::vector<int> vec){
-    for(unsigned int i=0;i<vec.size();i++){
-        vec[i] = vec[i]*2;
+#include <random>
+using namespace std;
+ 
+// Merges two subarrays of array[].
+// First subarray is arr[begin..mid]
+// Second subarray is arr[mid+1..end]
+void merge(int array[], int const left, int const mid,
+           int const right)
+{
+    std::cout << "test" << std::endl;
+     auto const subArrayOne = mid - left + 1;
+    auto const subArrayTwo = right - mid;
+ 
+    // Create temp arrays
+    auto *leftArray = new int[subArrayOne],
+         *rightArray = new int[subArrayTwo];
+ 
+    // Copy data to temp arrays leftArray[] and rightArray[]
+    for (auto i = 0; i < subArrayOne; i++)
+        leftArray[i] = array[left + i];
+    for (auto j = 0; j < subArrayTwo; j++)
+        rightArray[j] = array[mid + 1 + j];
+ 
+    auto indexOfSubArrayOne
+        = 0, // Initial index of first sub-array
+        indexOfSubArrayTwo
+        = 0; // Initial index of second sub-array
+    int indexOfMergedArray
+        = left; // Initial index of merged array
+ 
+    // Merge the temp arrays back into array[left..right]
+    while (indexOfSubArrayOne < subArrayOne
+           && indexOfSubArrayTwo < subArrayTwo) {
+        if (leftArray[indexOfSubArrayOne]
+            <= rightArray[indexOfSubArrayTwo]) {
+            array[indexOfMergedArray]
+                = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        }
+        else {
+            array[indexOfMergedArray]
+                = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
     }
-    return vec;
+  
+    while (indexOfSubArrayOne < subArrayOne) {
+        array[indexOfMergedArray]
+            = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+   
+    while (indexOfSubArrayTwo < subArrayTwo) {
+        array[indexOfMergedArray]
+            = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+    delete[] leftArray;
+    delete[] rightArray;
 }
+ 
 
+void mergeSort(int array[], int const begin, int const end)
+{
+    std::cout << "neue Liste" << std::endl;
+    if (begin >= end)
+        return; 
+ 
+    auto mid = begin + (end - begin) / 2;
+    mergeSort(array, begin, mid);
+    mergeSort(array, mid + 1, end);
+    merge(array, begin, mid, end);
+}
+ 
 
-int main(){
-    std::vector<int> vect = {{1,2,3,4}};
-    std::vector<int> ne = manis(vect);
-    for(auto x: ne){
-        std::cout << x << " ";
+void printArray(int A[], int size)
+{
+    for (auto i = 0; i < size; i++)
+        std::cout << A[i] << " ";
+}
+ 
+
+int main()
+{
+
+    
+    const int size = 60;
+    int arr[size];
+    std::random_device rd; // obtain a random number from hardware
+    std::mt19937 gen(rd()); // seed the generator
+    std::uniform_int_distribution<> distr(1,100); // define the range
+    for(int i=0;i<size;i++){
+        arr[i] = distr(gen);
     }
+    auto arr_size = sizeof(arr) / sizeof(arr[0]);
+ 
+    std::cout << "Given array is \n";
+    printArray(arr, arr_size);
+ 
+    mergeSort(arr, 0, arr_size - 1);
+ 
+    cout << "\nSorted array is \n";
+    printArray(arr, arr_size);
+    return 0;
 }
