fe690ea9533fb903b88dd46cb165cdd92122e7b3
Tobias.S <sieber.tobias8@gmail.com>
2022-10-12 14:13:23 +0200

    Returns Min of Ascending/Descending swaps needed

--- minswaps.cpp
+++ minswaps.cpp
@@ -1,8 +1,10 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 
-using namespace std;
 
-vector<string> split_string(string);
+
+
 
 int count(std::vector<std::pair<int,int>> a){
     int swap=0;
@@ -17,71 +19,36 @@ int count(std::vector<std::pair<int,int>> a){
 }
 return swap;
 }
-int minimumSwaps(vector<int> arr) {
+
+
+
+
+int lilysHomework(std::vector<int> arr) {
+
 std::vector<std::pair<int,int>> vec(arr.size());
-for(int i=0;i<arr.size();i++){
+for(unsigned int i=0;i<arr.size();i++){
     vec[i] = {arr[i],i};
 }
+int swap=0;
+int swapd = 0;
 std::sort(vec.begin(),vec.end());
-std::cout << count(vec) << std::endl;
-return count(vec);
+std::vector<std::pair<int,int>> vecd = vec;
+std::reverse(vecd.begin(),vecd.end());
+
+return std::min(count(vec),count(vecd));
 }
 
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    int n;
-    cin >> n;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    string arr_temp_temp;
-    getline(cin, arr_temp_temp);
-
-    vector<string> arr_temp = split_string(arr_temp_temp);
-
-    vector<int> arr(n);
-
-    for (int i = 0; i < n; i++) {
-        int arr_item = stoi(arr_temp[i]);
-
-        arr[i] = arr_item;
+    int n = 5;
+    std::cin >> n;
+    int val = 0;
+    std::vector<int> vec;
+    while(n!=0){
+        std::cin >> val;
+        vec.push_back(val);
+        n--;
     }
-
-    int res = minimumSwaps(arr);
-
-    fout << res << "\n";
-
-    fout.close();
-
+   std::cout << lilysHomework(vec) << std::endl;
     return 0;
-}
-
-vector<string> split_string(string input_string) {
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-        return x == y and x == ' ';
-    });
-
-    input_string.erase(new_end, input_string.end());
-
-    while (input_string[input_string.length() - 1] == ' ') {
-        input_string.pop_back();
-    }
-
-    vector<string> splits;
-    char delimiter = ' ';
-
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
-
-    while (pos != string::npos) {
-        splits.push_back(input_string.substr(i, pos - i));
-
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
-
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-    return splits;
 }
