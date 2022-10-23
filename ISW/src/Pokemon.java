9f71baa05b1b9b61c2023a7bf9ab7103b12bc2df
Tobias.S <sieber.tobias8@gmail.com>
2022-10-23 19:12:35 +0200

    Final push

---
+++ ISW/src/Pokemon.java
@@ -0,0 +1,54 @@
+//package de.uni.hd.isw.bigfolder;
+
+public class Pokemon {
+    private String _name;
+    Type _type;
+    int ID;
+    static int next; // static ermöglicht fortlaufendes zählen der ID
+    Pokemon(String name,Type type) {
+
+        this._name = name;
+        this._type = type;
+        ID = next;
+        next++;
+
+    }
+    public String get_name() {
+        return this._name;
+    }
+    public Type get_type() {
+        return this._type;
+    }
+    public void set_name(String _name) { //this zeigt dem Compiler das es sich um die Instanz des Objektes geht
+        this._name = _name;
+
+    }
+    public void set_Type(Type _type) {
+        this._type = _type;
+    }
+    public void show() {
+        System.out.println(this._name + " ist vom Typen " + _type);
+    }
+    
+    public String toString() {
+        return "Name: " + _name + " | Type:  " +  _type.toString()+ " | ID:   " + String.valueOf(ID);
+    }
+
+
+    public static void main(String[] args) {
+        Pokemon first; // Deklariert
+        first = new Pokemon("Glumanda",Type.FIRE);
+        first.show();
+        // Setze neue Werte
+        first.set_name("Glurak");
+        first.set_Type(Type.Poisen);
+        first.show();
+        Pokemon sec = new Pokemon("Bisasam",Type.Poisen);
+
+        String _first = first.toString();
+        String _sec = sec.toString();
+        System.out.println(_first);
+        System.out.println(_sec);
+    }
+
+}
--- ISW/src/PokemonTest.java
+++ ISW/src/PokemonTest.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 import org.junit.jupiter.api.Test;
 
 class PokemonTest {
-
+	
 	@Test
 	void test() {
 		int index_for_ID_testing  =1;
@@ -28,13 +28,13 @@ class PokemonTest {
 			}
 		}
 		
-		Pokemon show = new Pokemon("",Type.WATER);
-		System.out.println("Pruefe ID");
-		assertEquals(index_for_ID_testing-1,show.ID);
+		Pokemon show = new Pokemon("Shiggy",Type.WATER);
+		System.out.println("teste ID");
+		assertEquals(index_for_ID_testing-1,show.ID); // -1 da ID bei 0 startet
 		System.out.println("Es wurden genau 101 ID's generiert, OK \n");
-		System.out.println("Pruefe get_name");
+		System.out.println("teste get_name");
 		assertEquals(index_for_ID_testing,index_for_Name_testing);  
-		System.out.println("Es wurden genau 101 Namen generiert, OK \n ");
+		System.out.println("Es wurden genau 101 Namen generiert + 101 get_Name richtig aufgerufen, OK \n ");
 		
 		Type test = Type.WATER;
 		Pokemon TypeTest = new Pokemon("ISW rocks",test);
@@ -44,7 +44,11 @@ class PokemonTest {
 		System.out.println("Testing set_Name und set_Type");
 		
 		assertEquals(index_for_setMethod,index_for_ID_testing);
-		System.out.println("set_name und set_type konnte durchlaufen, OK");
+		System.out.println("set_name und set_type konnte durchlaufen, OK \n");
+			String str1 = "Name: Shiggy | Type:  WATER | ID:   100";
+		System.out.println("Testing to_String");
+		assertEquals(show.toString(),str1);
+		System.out.println("to_string gibt erwarteten Wert, OK");
 	}
 
 }
